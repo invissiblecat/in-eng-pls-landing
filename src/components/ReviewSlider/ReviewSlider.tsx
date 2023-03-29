db7d0af23a73196ca1124d2431c943b2cb09e662
invissiblecat <invica@list.ru>
2023-03-29 15:37:46 +0300

    review slider fix

--- src/components/ReviewSlider/ReviewSlider.tsx
+++ src/components/ReviewSlider/ReviewSlider.tsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/scrollbar";
 
+import Otziv1 from "../../assets/icons/otziv1.png";
 import { SectionTitle } from "../SectionTitle/SectionTitle";
 import { SliderNavigation } from "../SliderNavigation/SliderNavigation";
 import "./ReviewSlider.sass";
@@ -13,18 +14,26 @@ export const ReviewSlider: React.FC = () => {
   return (
     <div className="review-slider">
       <div className="wrapper">
-        <SectionTitle title="reviews" translation="отзывы" />
-        <Swiper
-          spaceBetween={1000}
-          slidesPerView={1}
-          onSlideChange={() => console.log("slide change")}
-        >
-          <SliderNavigation />
-          <SwiperSlide>slide 1</SwiperSlide>
-          <SwiperSlide>slide 2</SwiperSlide>
-          <SwiperSlide>slide 3</SwiperSlide>
-          <SwiperSlide>slide 4</SwiperSlide>
-        </Swiper>
+        <div className="slider-wrapper">
+          <SectionTitle title="reviews" translation="отзывы" />
+          <Swiper
+            spaceBetween={700}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SliderNavigation />
+            <SwiperSlide>
+              <img className="swiper-img" src={Otziv1} alt="review" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper-img" src={Otziv1} alt="review" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="swiper-img" src={Otziv1} alt="review" />
+            </SwiperSlide>
+            <SwiperSlide>slide 4</SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
